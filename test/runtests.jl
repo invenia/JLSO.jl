fa833ace1d3d75798c51d64360ee61666f4b1342
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-05-23 18:37:11 +0100

    reorganize the tests too

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,21 @@ using BSON
 using Dates
 using Distributed
 using InteractiveUtils
+using Memento
 using Random
 using Serialization
 using Test
-using JLSO
-using Memento
 
-@testset "JLSO.jl" begin
-    include("JLSO.jl")
+using JLSO
+using JLSO: JLSOFile, LOGGER
+
+# To test different types from common external packages
+using DataFrames
+using Distributions
+using TimeZones
+
+include("test_data_setup.jl")
+@testset "JLSO" begin
+    include("JLSOFile.jl")
+    include("file_io.jl")
 end
