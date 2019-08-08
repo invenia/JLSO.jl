a5c71ced8a8a4119fb520c71d1c7fe5556ebded3
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-08-08 19:57:42 +0000

    build based on 3aca0d7

---
+++ v1.1.0/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#JLSO-1",
+    "page": "Home",
+    "title": "JLSO",
+    "category": "section",
+    "text": "(Image: Stable) (Image: Dev) (Image: Build Status) (Image: Build Status) (Image: Codecov)JLSO wraps other formats (currently BSON), while storing key metadata such as the version of julia and all involved packages that were used in creating the file."
+},
+
+]}
