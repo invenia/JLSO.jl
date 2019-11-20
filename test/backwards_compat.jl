c12c772d2bc3fb67ea4d85b0b74ee20862e01b78
rofinn <rory.finnegan@gmail.com>
2019-11-19 19:44:28 -0600

    Add support for upgrading (and batch upgrading) files directly.

--- test/backwards_compat.jl
+++ test/backwards_compat.jl
@@ -1,64 +1,93 @@
-@testset "upgrade_jlso" begin
-    @testset "no change for current version" begin
-        d = Dict("metadata" => Dict("version" => v"3.0"))
-        new_d = @suppress_out upgrade_jlso(d)
-        @test new_d == Dict("metadata" => Dict("version" => v"3.0"))
-    end
+@testset "Upgrading" begin
+    @testset "JLSO.upgrade_jlso" begin
+        @testset "no change for current version" begin
+            d = Dict("metadata" => Dict("version" => v"3.0"))
+            new_d = @suppress_out upgrade_jlso(d)
+            @test new_d == Dict("metadata" => Dict("version" => v"3.0"))
+        end
 
-    @testset "upgrade 1.0" begin
-        d = Dict("metadata" => Dict("version" => v"1.0", "format"=>:serialize))
-        new_d = @suppress_out upgrade_jlso(d)
-        @test new_d["metadata"]["version"] == "3"
-        @test new_d["metadata"]["format"] == "julia_serialize"
-        @test new_d["metadata"]["compression"] == "none"
-        @test isempty(new_d["metadata"]["project"])
-        @test isempty(new_d["metadata"]["manifest"])
-        @test isempty(new_d["objects"])
-
-        @testset "Don't rename bson format" begin
-            d = Dict("metadata" => Dict("version" => v"1.0", "format"=>:bson))
+        @testset "upgrade 1.0" begin
+            d = Dict("metadata" => Dict("version" => v"1.0", "format"=>:serialize))
             new_d = @suppress_out upgrade_jlso(d)
             @test new_d["metadata"]["version"] == "3"
-            @test new_d["metadata"]["format"] == "bson"
+            @test new_d["metadata"]["format"] == "julia_serialize"
             @test new_d["metadata"]["compression"] == "none"
             @test isempty(new_d["metadata"]["project"])
             @test isempty(new_d["metadata"]["manifest"])
             @test isempty(new_d["objects"])
-        end
 
-        @testset "Invalid VersionNumber" begin
-            d = Dict(
-                "metadata" => Dict(
-                    "version" => v"1.0", "format" => :bson,
-                    # We don't intend to go back an make a 0.3.14 release at any point.
-                    "pkgs" => merge(pkgs, Dict("JLSO" => v"0.3.14")),
+            @testset "Don't rename bson format" begin
+                d = Dict("metadata" => Dict("version" => v"1.0", "format"=>:bson))
+                new_d = @suppress_out upgrade_jlso(d)
+                @test new_d["metadata"]["version"] == "3"
+                @test new_d["metadata"]["format"] == "bson"
+                @test new_d["metadata"]["compression"] == "none"
+                @test isempty(new_d["metadata"]["project"])
+                @test isempty(new_d["metadata"]["manifest"])
+                @test isempty(new_d["objects"])
+            end
+
+            @testset "Invalid VersionNumber" begin
+                d = Dict(
+                    "metadata" => Dict(
+                        "version" => v"1.0", "format" => :bson,
+                        # We don't intend to go back an make a 0.3.14 release at any point.
+                        "pkgs" => merge(pkgs, Dict("JLSO" => v"0.3.14")),
+                    )
                 )
-            )
 
-            new_d = @suppress_out upgrade_jlso(d)
-            @test !isempty(new_d["metadata"]["project"])
-            @test !isempty(new_d["metadata"]["manifest"])
+                new_d = @suppress_out upgrade_jlso(d)
+                @test !isempty(new_d["metadata"]["project"])
+                @test !isempty(new_d["metadata"]["manifest"])
 
-            # Test that the project.toml has the JLSO dep, but not a specific version
-            # (fallback when package versions don't exist in the registry)
-            _project = Pkg.TOML.parse(new_d["metadata"]["project"])
-            @test haskey(_project, "deps")
-            @test haskey(_project["deps"], "JLSO")
-            @test haskey(_project, "compat")
-            # Check that the compat section doesn't have our package version
-            @test !haskey(_project["compat"], "JLSO")
+                # Test that the project.toml has the JLSO dep, but not a specific version
+                # (fallback when package versions don't exist in the registry)
+                _project = Pkg.TOML.parse(new_d["metadata"]["project"])
+                @test haskey(_project, "deps")
+                @test haskey(_project["deps"], "JLSO")
+                @test haskey(_project, "compat")
+                # Check that the compat section doesn't have our package version
+                @test !haskey(_project["compat"], "JLSO")
+            end
         end
     end
-end
 
-# The below is how we saves the specimens for compat testing
-# JLSO.save("specimens/v1_serialize.jlso", datas; format=:serialize)
-# JLSO.save("specimens/v1_bson.jlso", datas; format=:bson)
+    # The below is how we saves the specimens for compat testing
+    # JLSO.save("specimens/v1_serialize.jlso", datas; format=:serialize)
+    # JLSO.save("specimens/v1_bson.jlso", datas; format=:bson)
 
-@testset "Can still load old files" begin
-    dir = joinpath(@__DIR__, "specimens")
-    @testset "$fn" for fn in readdir(dir)
-        jlso_data = @suppress_out JLSO.load(joinpath(dir, fn))
-        @test jlso_data == datas
+    @testset "Can still load old files" begin
+        dir = joinpath(@__DIR__, "specimens")
+        @testset "$fn" for fn in readdir(dir)
+            jlso_data = @suppress_out JLSO.load(joinpath(dir, fn))
+            @test jlso_data == datas
+        end
+    end
+
+    # Test upgrading files to avoid repeated upgrading
+    @testset "JLSO.upgrade" begin
+        src_dir = joinpath(@__DIR__, "specimens")
+        @testset "upgrade" begin
+            mktempdir() do dest_dir
+                @testset "$fn" for fn in readdir(src_dir)
+                    src = joinpath(src_dir, fn)
+                    dest = joinpath(dest_dir, fn)
+                    @suppress_out JLSO.upgrade(src, dest)
+                    jlso_data = JLSO.load(dest)
+                    @test jlso_data == datas
+                end
+            end
+        end
+        @testset "batch upgrade" begin
+            mktempdir() do dest_dir
+                fns = readdir(src_dir)
+                src = joinpath.(src_dir, fns)
+                dest = joinpath.(dest_dir, fns)
+
+                @suppress_out JLSO.upgrade(src, dest)
+                jlso_data = JLSO.load(first(dest))
+                @test jlso_data == datas
+            end
+        end
     end
 end
